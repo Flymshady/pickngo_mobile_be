34c71e6ca89d35940c1a0382bf8390551d7c6dfc
Štěpán Cellar <stepan.cellar@gmail.com>
2021-03-29 21:15:30 +0200

    User package - model, repo, serv, controll, config bean, mysql conn

--- src/main/java/cz/uhk/fim/bs/pickngo_mobile_be/PickngoMobileBeApplication.java
+++ src/main/java/cz/uhk/fim/bs/pickngo_mobile_be/PickngoMobileBeApplication.java
@@ -2,8 +2,10 @@ package cz.uhk.fim.bs.pickngo_mobile_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+        //(exclude = { SecurityAutoConfiguration.class })
 public class PickngoMobileBeApplication {
 
     public static void main(String[] args) {
