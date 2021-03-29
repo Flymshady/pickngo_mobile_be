90c1f4b7ea26380dd4438a0e06a25eb69ecea3cb
Štěpán Cellar <stepan.cellar@gmail.com>
2021-03-29 18:14:15 +0200

    Initial commit

---
+++ src/main/java/cz/uhk/fim/bs/pickngo_mobile_be/PickngoMobileBeApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.bs.pickngo_mobile_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickngoMobileBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickngoMobileBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/fim/bs/pickngo_mobile_be/PickngoMobileBeApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.bs.pickngo_mobile_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickngoMobileBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
