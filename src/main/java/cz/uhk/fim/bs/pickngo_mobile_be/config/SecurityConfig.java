ae556fc47f6bb110a4d783c3b21ecda2bda134b8
Štěpán Cellar <stepan.cellar@gmail.com>
2021-04-12 18:44:17 +0200

    security

--- src/main/java/cz/uhk/fim/bs/pickngo_mobile_be/config/SecurityConfig.java
+++ src/main/java/cz/uhk/fim/bs/pickngo_mobile_be/config/SecurityConfig.java
@@ -1,17 +1,20 @@
 package cz.uhk.fim.bs.pickngo_mobile_be.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure( HttpSecurity http ) throws Exception {
 
+     //   http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers( "/oauth2/**", "/login/**" ).permitAll()
                 .anyRequest().authenticated()
