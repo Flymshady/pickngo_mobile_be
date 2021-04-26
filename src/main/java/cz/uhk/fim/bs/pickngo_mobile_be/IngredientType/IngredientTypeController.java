4739781a139a5506bd0e8721754b0cea09efa172
Štěpán Cellar <stepan.cellar@gmail.com>
2021-04-26 20:53:41 +0200

    @CrossOrigin na controllery

--- src/main/java/cz/uhk/fim/bs/pickngo_mobile_be/IngredientType/IngredientTypeController.java
+++ src/main/java/cz/uhk/fim/bs/pickngo_mobile_be/IngredientType/IngredientTypeController.java
@@ -1,12 +1,14 @@
 package cz.uhk.fim.bs.pickngo_mobile_be.IngredientType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path="/ingredientType")
 public class IngredientTypeController {
