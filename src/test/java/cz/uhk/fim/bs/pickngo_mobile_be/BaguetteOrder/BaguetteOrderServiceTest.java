fdee1028fd8aad92b11fb0def0bf885dcf03351d
Štěpán Cellar <stepan.cellar@gmail.com>
2021-05-02 20:14:47 +0200

    BaguetteOrder serv testy (chybi confirm s date)

--- src/test/java/cz/uhk/fim/bs/pickngo_mobile_be/BaguetteOrder/BaguetteOrderServiceTest.java
+++ src/test/java/cz/uhk/fim/bs/pickngo_mobile_be/BaguetteOrder/BaguetteOrderServiceTest.java
@@ -4,20 +4,27 @@ import cz.uhk.fim.bs.pickngo_mobile_be.BaguetteItem.BaguetteItem;
 import cz.uhk.fim.bs.pickngo_mobile_be.BaguetteItem.BaguetteItemRepository;
 import cz.uhk.fim.bs.pickngo_mobile_be.Customer.Customer;
 import cz.uhk.fim.bs.pickngo_mobile_be.Customer.CustomerRepository;
+import cz.uhk.fim.bs.pickngo_mobile_be.Item.Item;
 import cz.uhk.fim.bs.pickngo_mobile_be.Item.ItemRepository;
 import cz.uhk.fim.bs.pickngo_mobile_be.Item.ItemService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -115,19 +122,238 @@ class BaguetteOrderServiceTest {
 
     @Test
     void removeBaguetteOrder() {
+        String email ="email";
+
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(1L, customer, 2.0, new Date(), 0,"note");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.of(baguetteOrder));
+
+        underTest.removeBaguetteOrder(baguetteOrder.getId(), email);
+        verify(baguetteOrderRepository).delete(baguetteOrder);
+    }
+
+    @Test
+    void ThrowRemoveBaguetteOrder() {
+        String email ="email";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(1L, customer, 2.0, new Date(), 0,"note");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.empty());
+        assertThatThrownBy(() ->underTest.removeBaguetteOrder(baguetteOrder.getId(), email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("chyba, uživatel nenalezen");
+        verify(baguetteOrderRepository, never()).delete(any());
+    }
+    @Test
+    void Throw2RemoveBaguetteOrder() {
+        String email ="email_";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(1L, customer, 2.0, new Date(), 0,"note");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.empty());
+
+        assertThatThrownBy(() ->underTest.removeBaguetteOrder(baguetteOrder.getId(), email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("Objednávka nenelazena");
+        verify(baguetteOrderRepository, never()).delete(any());
+    }
+    @Test
+    void Throw3RemoveBaguetteOrder() {
+        String email ="email";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(1L, customer, 2.0, new Date(), 1,"note");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.of(baguetteOrder));
+
+        assertThatThrownBy(() ->underTest.removeBaguetteOrder(baguetteOrder.getId(), email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("Objednávku nelze odstranit");
+        verify(baguetteOrderRepository, never()).delete(any());
     }
 
     @Test
     void updateBaguetteOrder() {
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note");
+        BaguetteOrder baguetteOrder2 = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note2");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.of(baguetteOrder));
+
+        underTest.updateBaguetteOrder(baguetteOrder2.getId(),baguetteOrder2.getNote(), email);
+        ArgumentCaptor<BaguetteOrder> baguetteOrderArgumentCaptor = ArgumentCaptor.forClass(BaguetteOrder.class);
+        verify(baguetteOrderRepository).save(baguetteOrderArgumentCaptor.capture());
+        BaguetteOrder capturedBaguetteOrder = baguetteOrderArgumentCaptor.getValue();
+
+        assertThat(capturedBaguetteOrder).isExactlyInstanceOf(BaguetteOrder.class);
+        assertThat(capturedBaguetteOrder.getNote()).isEqualTo(baguetteOrder2.getNote());
     }
 
     @Test
+    void ThrowUpdateBaguetteOrder() {
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note");
+        BaguetteOrder baguetteOrder2 = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note2");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.empty());
+        assertThatThrownBy(() ->underTest.updateBaguetteOrder(baguetteOrder.getId(),baguetteOrder2.getNote(), email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("chyba, uživatel nenalezen");
+        verify(baguetteOrderRepository, never()).save(any());
+    }
+
+    @Test
+    void Throw2UpdateBaguetteOrder() {
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note");
+        BaguetteOrder baguetteOrder2 = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note2");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.empty());
+
+        assertThatThrownBy(() ->underTest.updateBaguetteOrder(baguetteOrder.getId(),baguetteOrder2.getNote(), email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("Objednávka nenelazena");
+        verify(baguetteOrderRepository, never()).save(any());
+    }
+    @Test
+    void Throw3UpdateBaguetteOrder() {
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 1,"note");
+        BaguetteOrder baguetteOrder2 = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 1,"note2");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.empty());
+
+        assertThatThrownBy(() ->underTest.updateBaguetteOrder(baguetteOrder.getId(),baguetteOrder2.getNote(), email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("Objednávku nelze upravit");
+        verify(baguetteOrderRepository, never()).save(any());
+    }
+
+    @Test
+    @Disabled
     void confirmBaguetteOrder() {
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        String datestring = "02-05-2021, 18:54:35";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note");
+        BaguetteOrder baguetteOrder2 = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note2");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.of(baguetteOrder));
+
+        underTest.confirmBaguetteOrder(baguetteOrder2.getId(),datestring, email);
+        ArgumentCaptor<BaguetteOrder> baguetteOrderArgumentCaptor = ArgumentCaptor.forClass(BaguetteOrder.class);
+        verify(baguetteOrderRepository).save(baguetteOrderArgumentCaptor.capture());
+        BaguetteOrder capturedBaguetteOrder = baguetteOrderArgumentCaptor.getValue();
+
+        assertThat(capturedBaguetteOrder).isExactlyInstanceOf(BaguetteOrder.class);
+        assertThat(capturedBaguetteOrder.getDate().toString()).isEqualTo(datestring);
+    }
+    @Test
+    void WontConfirmBaguetteOrder() {
+        String email = "email";
+        Long baguetteOrderId = 1L;
+        String datestring = "02-05-2021, 18:54:35";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0, "note");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.empty());
+     //   given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.of(baguetteOrder));
+
+        assertThatThrownBy(() ->underTest.confirmBaguetteOrder(baguetteOrder.getId(), datestring, email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("chyba, uživatel nenalezen");
+        verify(baguetteOrderRepository, never()).save(any());
+
+    }
+    @Test
+    void Wont2ConfirmBaguetteOrder() {
+        String email = "email";
+        Long baguetteOrderId = 1L;
+        String datestring = "02-05-2021, 18:54:35";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0, "note");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.empty());
+
+        assertThatThrownBy(() ->underTest.confirmBaguetteOrder(baguetteOrder.getId(), datestring, email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("Objednávka nenelazena");
+        verify(baguetteOrderRepository, never()).save(any());
+    }
+    @Test
+    void Wont3ConfirmBaguetteOrder() {
+        String email = "email";
+        Long baguetteOrderId = 1L;
+        String datestring = "02-05-2021, 18:54:35";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 1, "note");
+
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findBaguetteOrderByIdAndCustomer_Email(baguetteOrder.getId(), email)).willReturn(Optional.of(baguetteOrder));
+
+        assertThatThrownBy(() ->underTest.confirmBaguetteOrder(baguetteOrder.getId(), datestring, email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("Objednávku nelze upravit");
+        verify(baguetteOrderRepository, never()).save(any());
     }
 
     @Test
     void createBaguetteOrder() {
+        List<BaguetteOrder> baguetteOrderList = new ArrayList<>();
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        String datestring = "02-05-2021, 18:54:35";
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note");
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findAllByStateAndCustomer_Email(0, email)).willReturn(Optional.empty());
+
+        underTest.createBaguetteOrder(email);
+        ArgumentCaptor<BaguetteOrder> baguetteOrderArgumentCaptor = ArgumentCaptor.forClass(BaguetteOrder.class);
+        verify(baguetteOrderRepository).save(baguetteOrderArgumentCaptor.capture());
+        BaguetteOrder capturedBaguetteOrder = baguetteOrderArgumentCaptor.getValue();
+
+        assertThat(capturedBaguetteOrder).isExactlyInstanceOf(BaguetteOrder.class);
+        assertThat(capturedBaguetteOrder.getState()).isEqualTo(baguetteOrder.getState());
+        assertThat(capturedBaguetteOrder.getCustomer()).isEqualTo(baguetteOrder.getCustomer());
+
     }
+    @Test
+    void ThrowCreateBaguetteOrder() {
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        Customer customer = new Customer("name", email, "emailshort");
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.empty());
+        assertThatThrownBy(() ->underTest.createBaguetteOrder(email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("chyba, uživatel nenalezen");
+        verify(baguetteOrderRepository, never()).save(any());
+
+    }
+    @Test
+    void Throw2CreateBaguetteOrder() {
+        List<BaguetteOrder> baguetteOrderList = new ArrayList<>();
+        String email ="email";
+        Long baguetteOrderId = 1L;
+        Customer customer = new Customer("name", email, "emailshort");
+        BaguetteOrder baguetteOrder = new BaguetteOrder(baguetteOrderId, customer, 2.0, new Date(), 0,"note");
+        baguetteOrderList.add(baguetteOrder);
+        given(customerRepository.findCustomerByEmail(email)).willReturn(Optional.of(customer));
+        given(baguetteOrderRepository.findAllByStateAndCustomer_Email(0, email)).willReturn(Optional.of(baguetteOrderList));
+
+        assertThatThrownBy(() ->underTest.createBaguetteOrder(email))
+                .isInstanceOf(ResponseStatusException.class).withFailMessage("nejprve dokončete objednávku");
+        verify(baguetteOrderRepository, never()).save(any());
+
+    }
+
 
     @Test
     void getBaguetteOrderActual() {
